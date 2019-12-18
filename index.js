196131c05ec60f325d30eaf7f741bf78088066d4
jeonghoheo <heojeongho1992@gmail.com>
2019-12-18 20:45:42 +0900

    add - 다대다 관계 기능 추가하기

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const typeDefs = gql`
     description: String
     category: PhotoCategory!
     postedBy: User!
+    taggedUsers: [User!]!
   }
 
   type User {
@@ -24,6 +25,7 @@ const typeDefs = gql`
     name: String
     avatar: String
     postedPhotos: [Photo!]!
+    inPhotos: [Photo!]!
   }
   # 2. allPhotos에서 Photo 타입을 반환한다.
   type Query {
@@ -81,6 +83,13 @@ let photos = [
   }
 ];
 
+let tags = [
+  { photoID: "1", userID: "gPlake" },
+  { photoID: "2", userID: "sSchmidt" },
+  { photoID: "2", userID: "mHattrup" },
+  { photoID: "2", userID: "gPlake" }
+];
+
 const resolvers = {
   Query: {
     totalPhotos: () => photos.length,
@@ -108,10 +117,21 @@ const resolvers = {
         return u.githubLogin === parent.githubUser;
       });
       return users.find(u => u.githubLogin === parent.githubUser);
-    }
+    },
+    taggedUsers: parent =>
+      tags
+        .filter(tag => tag.photoID === parent.id)
+        .map(tag => tag.userID)
+        .map(userID => users.find(u => u.githubLogin === userID))
   },
   User: {
-    postedPhotos: parent => photos.filter(p => p.githubUser === githubLogin)
+    postedPhotos: parent =>
+      photos.filter(p => p.githubUser === parent.githubLogin),
+    inPhotos: parent =>
+      tags
+        .filter(tag => tag.userID === parent.id)
+        .map(tag => tag.photoID)
+        .map(photoID => photos.find(p => p.id === photoID))
   }
 };
 
