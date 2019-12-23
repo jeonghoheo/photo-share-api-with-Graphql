a4973464ecb50258fd43bd9d44cee66105f48e7f
jeonghoheo <heojeongho1992@gmail.com>
2019-12-23 21:34:41 +0900

    add - 컨텍스트 사용하고 MongoDB연결해주기

--- index.js
+++ index.js
@@ -7,25 +7,43 @@ const { readFileSync } = require("fs");
 const typeDefs = readFileSync("./typeDefs.graphql", "UTF-8");
 const resolvers = require("./resolvers");
 
-const app = express();
+const MongoClient = require("mongodb").MongoClient;
+require("dotenv").config();
 
-// 2. 서버 인스턴스를 새로 만들기
-// 3. typeDefs(스키마)와 리졸버를 객체에 넣어 전달하기
-const server = new ApolloServer({
-  typeDefs,
-  resolvers
-});
+async function start() {
+  const app = express();
+  const MONGO_URL = process.env.DB_HOST;
+  const DB_NAME = process.env.DB_NAME;
 
-server.applyMiddleware({ app });
+  const client = await MongoClient.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
 
-const home = (req, res) => res.end("PhotoShare API에 오신 것을 환영합니다.");
+  const db = client.db(DB_NAME);
 
-app.get("/", home);
-app.get("/playground", expressPalyground({ endpoint: "/graphql" }));
+  const context = { db };
 
-const handleListening = () =>
-  console.log(
-    `GraphQL Server running @ http://localhost:4000${server.graphqlPath}`
-  );
+  // 2. 서버 인스턴스를 새로 만들기
+  // 3. typeDefs(스키마)와 리졸버를 객체에 넣어 전달하기
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context
+  });
+  server.applyMiddleware({ app });
 
-app.listen({ port: 4000 }, handleListening);
+  const home = (req, res) => res.end("PhotoShare API에 오신 것을 환영합니다.");
+
+  app.get("/", home);
+  app.get("/playground", expressPalyground({ endpoint: "/graphql" }));
+
+  const handleListening = () =>
+    console.log(
+      `GraphQL Server running @ http://localhost:4000${server.graphqlPath}`
+    );
+
+  app.listen({ port: 4000 }, handleListening);
+}
+
+start();
